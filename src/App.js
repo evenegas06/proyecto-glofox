e468bd1df1f6628737fbfc7d9f74f103d26319f7
Eduardo Venegas Rivera <evenegas06@hotmail.com>
2021-10-20 02:13:37 -0500

    prueba HashRouter para deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, HashRouter } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
@@ -12,14 +12,14 @@ import Contact from './pages/Contact';
 function App() {
   return (
 
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/contact" component={Contact} />
         <Route exact path="/about" component={AboutUs} />
         <Route component={NotFound} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
 
   );
 }
